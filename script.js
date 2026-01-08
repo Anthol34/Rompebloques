2375456f6f457df6a7e120175e06d8b416350797
Juan Mendoza <jutdavi777@gmail.com>
2026-01-08 09:48:09 -0500

    Implementación de controles de teclado y mouse para el movimiento de la paleta

--- script.js
+++ script.js
@@ -40,3 +40,79 @@ for (c = 0; c < brickColumnCount; c++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
+
+
+// Escuchadores de eventos para el teclado y el movimiento del mouse
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+// Anclar el movimiento de la paleta al movimiento del mouse
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if (relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2;
+    }
+}
+
+function keyDownHandler(e) {
+    if (e.keyCode === 39) {
+        rightPressed = true;
+    } else if (e.keyCode === 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode === 39) {
+        rightPressed = false;
+    } else if (e.keyCode === 37) {
+        leftPressed = false;
+    }
+}
+
+// Crear una función para crear la pelota
+function drawBall() {
+    ctx.beginPath();
+
+    // Centrada en la posición (x, y) con radio r = ballRadius,
+    // comenzando en 0 = ángulo inicial y terminando en Math.PI * 2 = ángulo final (en radianes)
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+
+    ctx.fillStyle = "lawngreen";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Crear una función para crear la paleta
+function drawPaddle() {
+    ctx.beginPath();
+
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "lawngreen";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Crear una función para dibujar los ladrillos
+function drawBricks() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+                let brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+                let brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#6600cc";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+
+
+
